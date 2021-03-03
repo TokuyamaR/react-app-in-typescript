2e0a0a3e4e1dbf2e6e187b68d5a436b009e70668
TokuyamaR <qpflapper.1007@gmail.com>
2021-03-04 08:46:10 +0900

    practice useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import Counter from "./Counter";
 
 interface AppProps {
   message?: string;
@@ -6,13 +7,17 @@ interface AppProps {
 
 // React.functionComponent : 関数自体にジェネリクスを用いて　型制約を行う
 // 関数自体に型制約を設定することで、defaultPropsのdescriptionのような思わぬデータの混入を防げる
-const App: React.FunctionComponent<AppProps> = ({message}) => {
-  return <div>{message}</div>;
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 };
 
 App.defaultProps = {
-  message: 'Hello defaultProps!!',
+  message: "Hello defaultProps!!",
   // description: 'This is App component.' // error
-}
+};
 
 export default App;
