5082883218c32316e8234db1fc18bf192af03701
TokuyamaR <qpflapper.1007@gmail.com>
2021-03-04 07:49:36 +0900

    add functional component in typescript basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+interface AppProps {
+  message?: string;
+}
+
+// React.functionComponent : 関数自体にジェネリクスを用いて　型制約を行う
+// 関数自体に型制約を設定することで、defaultPropsのdescriptionのような思わぬデータの混入を防げる
+const App: React.FunctionComponent<AppProps> = ({message}) => {
+  return <div>{message}</div>;
 };
+
+App.defaultProps = {
+  message: 'Hello defaultProps!!',
+  // description: 'This is App component.' // error
+}
 
 export default App;
